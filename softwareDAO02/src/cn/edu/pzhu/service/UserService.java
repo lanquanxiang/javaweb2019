1aaa52fc203452f80e1c517765e30b3dc7cb736b
lanquanxiang <lanquanxiang@gmail.com>
2021-10-14 17:59:25 +0800

    初始化业务层

---
+++ softwareDAO02/src/cn/edu/pzhu/service/UserService.java
@@ -0,0 +1,14 @@
+package cn.edu.pzhu.service;
+
+import cn.edu.pzhu.entity.Message;
+import cn.edu.pzhu.entity.User;
+
+public interface UserService {
+	//µÇÂ¼
+	public Message login(User user);
+	//×¢²á
+	
+	//ĞŞ¸ÄÃÜÂë
+	
+	//Ïú»§
+}
---
+++ softwareDAO02/src/cn/edu/pzhu/service/imp/UserServiceImp.java
@@ -0,0 +1,15 @@
+package cn.edu.pzhu.service.imp;
+
+import cn.edu.pzhu.entity.Message;
+import cn.edu.pzhu.entity.User;
+import cn.edu.pzhu.service.UserService;
+
+public class UserServiceImp implements UserService{
+
+	@Override
+	public Message login(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
